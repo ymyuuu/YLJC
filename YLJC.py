11b7d97e1b37dd5e083fc618aeae773c17cf9ba6
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-09-12 19:35:09 +0800

    Update YLJC.py

--- YLJC.py
+++ YLJC.py
@@ -133,7 +133,7 @@ def run_script():
 # 主流程
 remaining_traffic = check_traffic()
 
-if remaining_traffic is not None and remaining_traffic < 5:
+if remaining_traffic is not None and remaining_traffic < 50:
     print("剩余流量不足 5G，开始执行刷取...")
     if run_script():
         new_remaining_traffic = check_traffic()
